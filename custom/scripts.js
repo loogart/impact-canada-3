a74ac075f940dd430d0c320f77a1a368321ba01d
Loogart <chris@loogart.com>
2024-08-08 14:05:47 -0400

    images, templating, topics page
    - templated html pages for prototyping purposes
    - exported all images from figma for prototype
    - create topics page

--- custom/scripts.js
+++ custom/scripts.js
@@ -1,4 +1,24 @@
+// Function to load HTML file content
+function loadHTML(file, element, replace = false) {
+fetch(file)
+    .then(response => response.text())
+    .then(data => {
+    const target = document.querySelector(element);
+    if (replace) {
+        target.outerHTML = data;
+    } else {
+        target.innerHTML = data;
+    }
+    })
+    .catch(error => console.error('Error loading HTML:', error));
+}
+
+// Load head, header, and footer
+loadHTML('html/head.html', 'head', true);
+loadHTML('html/header.html', '#header-placeholder');
+loadHTML('html/footer.html', '#footer-placeholder');
+
 AOS.init();
 
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Impact Canada";
+//const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Impact Canada";
